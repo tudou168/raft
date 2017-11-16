0943d891f6e5f00a31037ac1ed51cc4a34c9a01d
banxia <466862016@163.com>
2017-11-16 21:47:47 +0800

    添加 选举监听器

---
+++ raft-core/src/main/java/com/tongbanjie/raft/core/protocol/ElectionResponseTuple.java
@@ -0,0 +1,39 @@
+package com.tongbanjie.raft.core.protocol;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.io.Serializable;
+
+/***
+ *
+ * @author banxia
+ * @date 2017-11-16 21:21:19
+ */
+public class ElectionResponseTuple implements Serializable {
+
+    private ElectionResponse electionResponse;
+
+    private boolean success;
+
+    public ElectionResponse getElectionResponse() {
+        return electionResponse;
+    }
+
+    public void setElectionResponse(ElectionResponse electionResponse) {
+        this.electionResponse = electionResponse;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+    }
+}
